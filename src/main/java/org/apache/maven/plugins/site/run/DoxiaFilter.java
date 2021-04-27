7c43482750baf3b018030af48253dc020a1ea9d7
dcruette <dcruette@qualitesys.com>
2021-04-27 20:25:58 +0200

    [MSITE-872] Update DoxiaFilter.java

--- src/main/java/org/apache/maven/plugins/site/run/DoxiaFilter.java
+++ src/main/java/org/apache/maven/plugins/site/run/DoxiaFilter.java
@@ -39,7 +39,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Locale;
