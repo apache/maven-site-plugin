d6c4750344ca702d5932c3210d97bde2dc42398a
Michael Osipov <michaelo@apache.org>
2023-03-19 22:44:45 +0100

    Upgrade to Parent 39 in full-reporting IT

--- src/it/projects/full-reporting/src/main/java/org/apache/maven/plugins/site/its/App.java
+++ src/it/projects/full-reporting/src/main/java/org/apache/maven/plugins/site/its/App.java
@@ -1,5 +1,3 @@
-package org.apache.maven.plugins.site.its;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,26 +16,24 @@ package org.apache.maven.plugins.site.its;
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.apache.maven.plugins.site.its;
 
 /**
  * Hello world!
  *
  */
-public class App
-{
+public class App {
     /**
      * main
      */
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     /**
      * foo
      */
-    public void foo()
-    {
-        System.out.println( "foo" );
+    public void foo() {
+        System.out.println("foo");
     }
 }
