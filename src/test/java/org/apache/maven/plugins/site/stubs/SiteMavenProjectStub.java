5e29b1fac3f3c9eeb85cb8eeeca36242cb4ecbb1
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2024-07-21 18:38:25 +0000

    Use try with resources to avoid deprecated cclass (#200)

--- src/test/java/org/apache/maven/plugins/site/stubs/SiteMavenProjectStub.java
+++ src/test/java/org/apache/maven/plugins/site/stubs/SiteMavenProjectStub.java
@@ -19,15 +19,16 @@
 package org.apache.maven.plugins.site.stubs;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.apache.maven.model.DistributionManagement;
 import org.apache.maven.model.Site;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
-import org.codehaus.plexus.util.IOUtil;
-import org.codehaus.plexus.util.ReaderFactory;
-import org.codehaus.plexus.util.xml.XmlStreamReader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 /**
  * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
@@ -41,24 +42,21 @@ public class SiteMavenProjectStub extends MavenProjectStub {
     public SiteMavenProjectStub(String projectName) {
         basedir = new File(super.getBasedir() + "/src/test/resources/unit/" + projectName);
 
-        XmlStreamReader reader = null;
-        try {
-            reader = ReaderFactory.newXmlReader(new File(getBasedir(), "pom.xml"));
-            setModel(new MavenXpp3Reader().read(reader));
-            reader.close();
-        } catch (Exception e) {
+        File pom = new File(getBasedir(), "pom.xml");
+        try (InputStream in = new FileInputStream(pom)) {
+            setModel(new MavenXpp3Reader().read(in));
+            Site site = new Site();
+            site.setId("localhost");
+            distributionManagement.setSite(site);
+        } catch (IOException | XmlPullParserException e) {
             throw new RuntimeException(e);
-        } finally {
-            IOUtil.close(reader);
         }
-        Site site = new Site();
-        site.setId("localhost");
-        distributionManagement.setSite(site);
     }
 
     /**
      * @see org.apache.maven.project.MavenProject#getName()
      */
+    @Override
     public String getName() {
         return getModel().getName();
     }
@@ -66,6 +64,7 @@ public class SiteMavenProjectStub extends MavenProjectStub {
     /**
      * @see org.apache.maven.project.MavenProject#getProperties()
      */
+    @Override
     public Properties getProperties() {
         return new Properties();
     }
@@ -76,6 +75,7 @@ public class SiteMavenProjectStub extends MavenProjectStub {
     }
 
     /** {@inheritDoc} */
+    @Override
     public File getBasedir() {
         return basedir;
     }
