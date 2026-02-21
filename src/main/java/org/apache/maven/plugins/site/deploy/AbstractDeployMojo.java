apache/maven-site-plugin

commit e414d23496e5bd3949774f2f8267e93eebf69f10

[MSITE-920] Remove Maven 2 compat code
This closes #121

src/main/java/org/apache/maven/plugins/site/deploy/AbstractDeployMojo.java
@@ -26,13 +26,11 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.DistributionManagement;
 import org.apache.maven.model.Site;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.site.AbstractSiteMojo;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Proxy;
-import org.apache.maven.settings.Server;
 import org.apache.maven.settings.Settings;
 import org.apache.maven.settings.crypto.DefaultSettingsDecryptionRequest;
 import org.apache.maven.settings.crypto.SettingsDecrypter;
@@ -52,17 +50,11 @@ import org.apache.maven.wagon.proxy.ProxyInfo;
 import org.apache.maven.wagon.repository.Repository;
 import org.codehaus.plexus.PlexusConstants;
 import org.codehaus.plexus.PlexusContainer;
-import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
-import org.codehaus.plexus.component.configurator.ComponentConfigurator;
-import org.codehaus.plexus.component.repository.exception.ComponentLifecycleException;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
-import org.codehaus.plexus.configuration.PlexusConfiguration;
-import org.codehaus.plexus.configuration.xml.XmlPlexusConfiguration;
 import org.codehaus.plexus.context.Context;
 import org.codehaus.plexus.context.ContextException;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
 import org.codehaus.plexus.util.StringUtils;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -302,8 +294,6 @@ public abstract class AbstractDeployMojo
 
         try
         {
-            configureWagon( wagon, repository.getId(), settings, container, getLog() );
-
             SettingsDecrypter settingsDecrypter = container.lookup( SettingsDecrypter.class );
 
             ProxyInfo proxyInfo = getProxy( repository, settingsDecrypter );
@@ -318,10 +308,6 @@ public abstract class AbstractDeployMojo
         catch ( ComponentLookupException cle )
         {
             throw new MojoExecutionException( "Unable to lookup SettingsDecrypter: " + cle.getMessage(), cle );
-        }
-        catch ( TransferFailedException e )
-        {
-            throw new MojoExecutionException( "Unable to configure Wagon: '" + repository.getProtocol() + "'", e );
         }
         finally
         {
@@ -621,65 +607,6 @@ public abstract class AbstractDeployMojo
         }
         getLog().debug( "getProxy 'protocol': " + protocol + " no ProxyInfo found" );
         return null;
-    }
-
-    /**
-     * Configure the Wagon with the information from serverConfigurationMap ( which comes from settings.xml )
-     *
-     * @todo Remove when {@link WagonManager#getWagon(Repository) is available}. It's available in Maven 2.0.5.
-     */
-    private static void configureWagon( Wagon wagon, String repositoryId, Settings settings, PlexusContainer container,
-                                        Log log )
-        throws TransferFailedException
-    {
-        log.debug( " configureWagon " );
-
-        // MSITE-25: Make sure that the server settings are inserted
-        for ( Server server : settings.getServers() )
-        {
-            String id = server.getId();
-
-            log.debug( "configureWagon server " + id );
-
-            if ( id != null && id.equals( repositoryId ) && ( server.getConfiguration() != null ) )
-            {
-                final PlexusConfiguration plexusConf =
-                    new XmlPlexusConfiguration( (Xpp3Dom) server.getConfiguration() );
-
-                ComponentConfigurator componentConfigurator = null;
-                try
-                {
-                    componentConfigurator =
-                        (ComponentConfigurator) container.lookup( ComponentConfigurator.ROLE, "basic" );
-                    componentConfigurator.configureComponent( wagon, plexusConf, container.getContainerRealm() );
-                }
-                catch ( final ComponentLookupException e )
-                {
-                    throw new TransferFailedException(
-                        "While configuring wagon for \'" + repositoryId + "\': Unable to lookup wagon configurator."
-                            + " Wagon configuration cannot be applied.", e );
-                }
-                catch ( ComponentConfigurationException e )
-                {
-                    throw new TransferFailedException( "While configuring wagon for \'" + repositoryId
-                                                           + "\': Unable to apply wagon configuration.", e );
-                }
-                finally
-                {
-                    if ( componentConfigurator != null )
-                    {
-                        try
-                        {
-                            container.release( componentConfigurator );
-                        }
-                        catch ( ComponentLifecycleException e )
-                        {
-                            log.error( "Problem releasing configurator - ignoring: " + e.getMessage() );
-                        }
-                    }
-                }
-            }
-        }
     }
 
     /**

src/main/java/org/apache/maven/plugins/site/render/ReportDocumentRenderer.java
@@ -26,8 +26,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.List;
@@ -224,10 +222,6 @@ public class ReportDocumentRenderer
                 // extended multi-page API
                 ( (MavenMultiPageReport) report ).generate( mainSink, multiPageSinkFactory, locale );
             }
-            else if ( generateMultiPage( locale, multiPageSinkFactory, mainSink ) )
-            {
-                // extended multi-page API for Maven 2.2, only accessible by reflection API
-            }
             else
             {
                 // old single-page-only API
@@ -308,39 +302,6 @@ public class ReportDocumentRenderer
         catch ( IOException e )
         {
             throw new RendererException( "Cannot create writer to " + outputName, e );
-        }
-    }
-
-    /**
-     * Try to generate report with extended multi-page API.
-     *
-     * @return <code>true</code> if the report was compatible with the extended API
-     */
-    private boolean generateMultiPage( Locale locale, SinkFactory sf, Sink sink )
-        throws MavenReportException
-    {
-        try
-        {
-            // MavenMultiPageReport is not in Maven Core, then the class is different in site plugin and in each report
-            // plugin: only reflection can let us invoke its method
-            Method generate =
-                report.getClass().getMethod( "generate", Sink.class, SinkFactory.class, Locale.class );
-
-            generate.invoke( report, sink, sf, locale );
-
-            return true;
-        }
-        catch ( SecurityException se )
-        {
-            return false;
-        }
-        catch ( NoSuchMethodException nsme )
-        {
-            return false;
-        }
-        catch ( IllegalArgumentException | IllegalAccessException | InvocationTargetException ite )
-        {
-            throw new MavenReportException( "error while invoking generate on " + report.getClass(), ite );
         }
     }
 

commit 237762871ab13094794364757ac158af89d3c7df

Configure Wagon from settings.xml
This regressed with
https://github.com/apache/maven-site-plugin/commit/e414d23496e5bd3949774f2f8267e93eebf69f10

This closes #1217

pom.xml
@@ -284,6 +284,14 @@ under the License.
       <version>${mavenVersion}</version>
       <scope>provided</scope>
     </dependency>
+    <!-- reusing component to configure Wagon -->
+    <dependency>
+      <groupId>org.apache.maven.resolver</groupId>
+      <artifactId>maven-resolver-transport-wagon</artifactId>
+      <version>1.4.1</version>
+      <!-- version shipping with Maven 3.6.3-->
+      <scope>compile</scope>
+    </dependency>
 
     <dependency>
       <groupId>org.apache.maven</groupId>

src/main/java/org/apache/maven/plugins/site/deploy/AbstractDeployMojo.java
@@ -55,6 +55,9 @@ import org.apache.maven.wagon.authorization.AuthorizationException;
 import org.apache.maven.wagon.observers.Debug;
 import org.apache.maven.wagon.proxy.ProxyInfo;
 import org.apache.maven.wagon.repository.Repository;
+import org.codehaus.plexus.configuration.xml.XmlPlexusConfiguration;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.eclipse.aether.transport.wagon.WagonConfigurator;
 
 /**
  * Abstract base class for deploy mojos.
@@ -129,6 +132,8 @@ public abstract class AbstractDeployMojo extends AbstractSiteMojo {
     @Inject
     SettingsDecrypter settingsDecrypter;
 
+    @Inject
+    private WagonConfigurator wagonConfigurator;
     /**
      * {@inheritDoc}
      */
@@ -284,6 +289,28 @@ public abstract class AbstractDeployMojo extends AbstractSiteMojo {
         if (!wagon.supportsDirectoryCopy()) {
             throw new MojoExecutionException(
                     "Wagon protocol '" + repository.getProtocol() + "' doesn't support directory copying");
+        }
+        // retrieve relevant settings
+        Server server = settings.getServer(repository.getId());
+        // taken over from
+        // https://github.com/apache/maven/blob/18a52647884dcc822eb04bf5a3265a21dc83256c/maven-core/src/main/java/org/apache/maven/internal/aether/DefaultRepositorySystemSessionFactory.java#L242
+        if (server != null && server.getConfiguration() != null) {
+            Xpp3Dom dom = (Xpp3Dom) server.getConfiguration();
+            for (int i = dom.getChildCount() - 1; i >= 0; i--) {
+                Xpp3Dom child = dom.getChild(i);
+                if ("wagonProvider".equals(child.getName())) {
+                    dom.removeChild(i);
+                }
+            }
+            XmlPlexusConfiguration config = new XmlPlexusConfiguration(dom);
+            try {
+                // use Wagon configurator from Resolver
+                wagonConfigurator.configure(wagon, config);
+            } catch (Exception e) {
+                throw new MojoExecutionException(
+                        "Error configuring wagon with server configuration for server id '" + repository.getId() + "'",
+                        e);
+            }
         }
         return wagon;
     }